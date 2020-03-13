43ccfff0c148652d7e4c07573260db798fa0003d
Matias Schroder <matischroder99@gmail.com>
2020-03-13 01:21:40 -0300

    Eliminacion de contador e intento de musik

--- src/timer.js
+++ src/timer.js
@@ -2,36 +2,47 @@ import React, { Component } from "react";
 import "./timer.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import laPalabra from "./Palabras.json";
-import "./ReactWatch";
-var contador = 10;
 
 class Timer extends Component {
+  props = {
+    url: "./base.mp3"
+  };
   state = {
     minutes: 0,
-    seconds: 0,
-    palabra: 1
+    seconds: 1,
+    palabra: null,
+    play: false
   };
 
+  audio = new Audio(this.props.url);
+
+  togglePlay = () => {
+    this.setState({}, () => {
+      this.audio.play();
+    });
+  };
   componentDidMount() {
     this.myInterval = setInterval(() => {
       const { seconds, minutes } = this.state;
-      contador = contador - 1;
       if (seconds > 0) {
         this.setState(({ seconds }) => ({
           seconds: seconds - 1
         }));
-        if (contador <= 0) {
-          let random = Math.floor(Math.random() * 9822 + 1);
-          this.setState(({}) => ({
-            palabra: random
-          }));
-          contador = 10;
-        }
+      }
+      if (seconds % 10 === 1 && seconds !== 1) {
+        let random = Math.floor(Math.random() * 9822 + 1);
+        this.setState(({}) => ({
+          palabra: random
+        }));
       }
       if (seconds === 0) {
         if (minutes === 0) {
           clearInterval(this.myInterval);
         } else {
+          let random = Math.floor(Math.random() * 9822 + 1);
+          this.setState(({}) => ({
+            palabra: random
+          }));
           this.setState(({ minutes }) => ({
             minutes: minutes - 1,
             seconds: 59
@@ -40,46 +51,32 @@ class Timer extends Component {
       }
     }, 1000);
   }
-  /*
-  componentePalabra() {
-    const { seconds } = this.state;
-    let random = Math.floor(Math.random() * 9822 + 1);
-    if (seconds === 50)
-      this.setState(({}) => ({
-        palabra: random
-      }));
-  }
-  S
+
   componentWillUnmount() {
     clearInterval(this.myInterval);
   }
-  
-  comenzarDeCero() {
-    this.myInterval = () => {
-      this.setState(({ minutes }) => ({
-        minutes: 1
-      }));
-      this.setState(({ seconds }) => ({
-        seconds: 0
-      }));
-    };
-  }
-*/
+
   render() {
     return (
       <div class="container">
+        <h1>
+          {this.state.palabra === null
+            ? null
+            : laPalabra[this.state.palabra].Palabras}
+        </h1>
         <button
           onClick={
+            (this.togglePlay,
             this.state.minutes > 0 || this.state.seconds > 0
               ? null
               : (this.myInterval = () => {
                   this.setState(({}) => ({
-                    minutes: 0
+                    minutes: 1
                   }));
                   this.setState(({}) => ({
-                    seconds: 59
+                    seconds: 0
                   }));
-                })
+                }))
           }
           type="button"
           class="btn btn-dark"
@@ -96,7 +93,6 @@ class Timer extends Component {
               : this.state.seconds}
           </h1>
         )}
-        <h1>{laPalabra[this.state.palabra].Palabras}</h1>
       </div>
     );
   }
